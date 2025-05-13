3c721e9e92c5536e4be8fdf25e97f5c402f112a5
Muhammad Yahya <yahya@wightcode.co.uk>
2025-05-13 21:52:29 +0100

    initial commit

---
+++ src/test/java/com/retailstore/feedback/FeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailstore.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
